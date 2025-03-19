4fb0973991c18057d1979ae27f2dbe4ccd0a36b3
KobiManuel <Emmakobi91@gmail.com>
2025-03-19 16:45:39 +0100

    fix(functionality): :bug: updated next.config and url route

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'example.com'], // Add any other image domains you need
+    domains: ['res.cloudinary.com', 'images.unsplash.com', 'example.com'], // Add any other image domains you need
   },
 }
 
--- src/app/customers/components/navbar.tsx
+++ src/app/customers/components/navbar.tsx
@@ -1,36 +1,36 @@
-'use client'
+"use client";
 
-import React, { useState, useEffect } from 'react'
-import { cn } from '@/lib/utils'
-import { NavbarHoverCard } from './nav-hover-card'
-import { usePathname, useRouter } from 'next/navigation'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { Hamburger } from '../../../../public/assets/icons/hamburger'
-import { useMediaQuery } from 'usehooks-ts'
-import { Dancing_Script } from 'next/font/google'
-import { signOut, useSession } from 'next-auth/react'
-import { User } from '../../../../public/assets/icons/User'
-import { motion, AnimatePresence } from 'framer-motion'
-import useFormErrorStore from '@/store/customer'
+import React, { useState, useEffect } from "react";
+import { cn } from "@/lib/utils";
+import { NavbarHoverCard } from "./nav-hover-card";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Hamburger } from "../../../../public/assets/icons/hamburger";
+import { useMediaQuery } from "usehooks-ts";
+import { Dancing_Script } from "next/font/google";
+import { signOut, useSession } from "next-auth/react";
+import { User } from "../../../../public/assets/icons/User";
+import { motion, AnimatePresence } from "framer-motion";
+import useFormErrorStore from "@/store/customer";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+} from "@/components/ui/dropdown-menu";
 import {
   ChevronDown,
   ListOrderedIcon,
   LogOut,
   MessageSquareCodeIcon,
   User2,
-} from 'lucide-react'
-import { TNavItem } from '@/types/nav-types'
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
-import MobileMenu from './menu-sheet'
-import Image from 'next/image'
+} from "lucide-react";
+import { TNavItem } from "@/types/nav-types";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import MobileMenu from "./menu-sheet";
+import Image from "next/image";
 
 // const dancingScript = Dancing_Script({
 //   subsets: ['latin'],
@@ -39,52 +39,52 @@ import Image from 'next/image'
 // })
 
 type Props = {
-  mobileNavItems: TNavItem[]
-  navItems: TNavItem[]
-  ctaLink: string
-  classNames?: Record<string, string>
-}
+  mobileNavItems: TNavItem[];
+  navItems: TNavItem[];
+  ctaLink: string;
+  classNames?: Record<string, string>;
+};
 
 const authenticatedNavItems = [
   {
-    label: 'My Profile',
-    href: '/customers/dashboard/profile',
+    label: "My Profile",
+    href: "/customers/dashboard/profile",
     subItems: [
-      { label: 'Update Profile', href: '/' },
-      { label: 'See Orders', href: '/' },
-      { label: 'Track', href: '/' },
+      { label: "Update Profile", href: "/" },
+      { label: "See Orders", href: "/" },
+      { label: "Track", href: "/" },
     ],
   },
   {
-    label: 'My Orders',
-    href: '/customers/dashboard/orders',
+    label: "My Orders",
+    href: "/customers/dashboard/orders",
   },
-]
+];
 
 const Navbar = ({ navItems, classNames }: Props) => {
-  const { data: session, status } = useSession()
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
-  const [firstName, setFirstName] = useState('')
-  const [isScrolled, setIsScrolled] = useState(false)
-  const pathname = usePathname()
-  const isMobile = useMediaQuery('(max-width: 1023px)')
-  const isSmallSCreen = useMediaQuery('(max-width: 768px)')
+  const { data: session, status } = useSession();
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const [firstName, setFirstName] = useState("");
+  const [isScrolled, setIsScrolled] = useState(false);
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 1023px)");
+  const isSmallSCreen = useMediaQuery("(max-width: 768px)");
 
-  const { isEmptyForm, errorMessage, resetFormError } = useFormErrorStore()
+  const { isEmptyForm, errorMessage, resetFormError } = useFormErrorStore();
   useEffect(() => {
-    let timer: NodeJS.Timeout
+    let timer: NodeJS.Timeout;
 
     if (isEmptyForm) {
       timer = setTimeout(() => {
-        resetFormError()
-      }, 3000)
+        resetFormError();
+      }, 3000);
     }
 
     return () => {
-      if (timer) clearTimeout(timer)
-    }
-  }, [isEmptyForm, resetFormError])
+      if (timer) clearTimeout(timer);
+    };
+  }, [isEmptyForm, resetFormError]);
 
   useEffect(() => {
     if (session?.user) {
@@ -92,60 +92,60 @@ const Navbar = ({ navItems, classNames }: Props) => {
         session.user.firstName
           ? session.user.firstName.charAt(0).toUpperCase() +
               session.user.firstName.slice(1)
-          : ''
-      )
+          : ""
+      );
     }
-  }, [session])
+  }, [session]);
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollPosition = window.scrollY
-      const viewportHeight = window.innerHeight
+      const scrollPosition = window.scrollY;
+      const viewportHeight = window.innerHeight;
 
-      setIsScrolled(scrollPosition > viewportHeight)
-    }
+      setIsScrolled(scrollPosition > viewportHeight);
+    };
 
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
       <AnimatePresence>
         {isEmptyForm && (
           <motion.div
-            className='fixed top-0 left-0 w-full bg-[#C1121F] text-white h-[60px] z-40 flex items-center justify-center shadow-md'
+            className="fixed top-0 left-0 w-full bg-[#C1121F] text-white h-[60px] z-40 flex items-center justify-center shadow-md"
             initial={{ y: -60 }}
             animate={{ y: isMobile ? 84 : 96 }}
             exit={{ y: -60 }}
-            transition={{ duration: 0.7, ease: 'easeInOut' }}
+            transition={{ duration: 0.7, ease: "easeInOut" }}
           >
-            <p className='text-center font-medium px-4'>⚠⚠⚠ {errorMessage}</p>
+            <p className="text-center font-medium px-4">⚠⚠⚠ {errorMessage}</p>
           </motion.div>
         )}
       </AnimatePresence>
       <nav
         className={cn(
-          'py-6 px-8 top-0 left-0 right-0 w-full z-50 transition-all duration-300 ease-in-out',
-          isScrolled ? ' bg-white/80 backdrop-blur-sm' : 'bg-white shadow-md',
-          isSmallSCreen || pathname !== '/' ? 'relative' : 'fixed',
-          'border-b border-gray-200'
+          "py-6 px-8 top-0 left-0 right-0 w-full z-50 transition-all duration-300 ease-in-out",
+          isScrolled ? " bg-white/80 backdrop-blur-sm" : "bg-white shadow-md",
+          isSmallSCreen || pathname !== "/" ? "relative" : "fixed",
+          "border-b border-gray-200"
         )}
       >
-        <div className='flex items-center justify-between max-w-7xl mx-auto'>
-          <div className=''>
-            <Link href='/'>
+        <div className="flex items-center justify-between max-w-7xl mx-auto">
+          <div className="">
+            <Link href="/">
               <Image
-                src='/assets/images/memo-logo.svg'
-                alt='logo'
+                src="/assets/images/memo-logo.svg"
+                alt="logo"
                 width={100}
                 height={100}
               />
             </Link>
           </div>
-          <div className='hidden gap-[85px] lg:flex'>
-            <ul className='flex items-center gap-8'>
-              {(status === 'authenticated'
+          <div className="hidden gap-[85px] lg:flex">
+            <ul className="flex items-center gap-8">
+              {(status === "authenticated"
                 ? authenticatedNavItems
                 : navItems
               ).map((item) => {
@@ -157,65 +157,65 @@ const Navbar = ({ navItems, classNames }: Props) => {
                       title={item.label}
                       className={cn(
                         pathname === item.href
-                          ? ''
-                          : 'text-[#370E06] hover:text-primary',
+                          ? ""
+                          : "text-[#370E06] hover:text-primary",
                         classNames?.navItem
                       )}
                     />
-                  )
+                  );
                 }
                 return (
                   <li key={item.href}>
                     <Link
                       href={item.href}
                       className={cn(
-                        'text-[#370E06] font-medium hover:text-primary',
-                        pathname === item.href ? 'font-bold' : '',
+                        "text-[#370E06] font-medium hover:text-primary",
+                        pathname === item.href ? "font-bold" : "",
                         classNames?.navItem
                       )}
                     >
                       {item.label}
                     </Link>
                   </li>
-                )
+                );
               })}
             </ul>
 
-            <div className='flex flex-row gap-6'>
-              <div className='flex'>
-                <div className='m-2'>
+            <div className="flex flex-row gap-6">
+              <div className="flex">
+                <div className="m-2">
                   <User />
                 </div>
 
-                <div className='flex flex-col '>
+                <div className="flex flex-col ">
                   <p>Hello,</p>
-                  {status === 'authenticated' ? (
+                  {status === "authenticated" ? (
                     <DropdownMenu>
                       <DropdownMenuTrigger>
-                        <div className='font-bold flex justify-center items-center gap-1'>
-                          <p className='font-bold hover:text-primary'>
-                            {firstName || 'User'}
+                        <div className="font-bold flex justify-center items-center gap-1">
+                          <p className="font-bold hover:text-primary">
+                            {firstName || "User"}
                           </p>
                           <ChevronDown size={20} />
                         </div>
                       </DropdownMenuTrigger>
                       <DropdownMenuContent>
                         <DropdownMenuSeparator />
-                        <Link href='/customers/dashboard/profile'>
+                        <Link href="/customers/dashboard/profile">
                           <DropdownMenuItem>
                             <User2 />
                             My Profile
                           </DropdownMenuItem>
                         </Link>
                         <DropdownMenuSeparator />
-                        <Link href='/customers/dashboard/orders'>
+                        <Link href="/customers/dashboard/orders">
                           <DropdownMenuItem>
                             <ListOrderedIcon />
                             My Orders
                           </DropdownMenuItem>
                         </Link>
                         <DropdownMenuSeparator />
-                        <Link href='/customers/dashboard/messages'>
+                        <Link href="/customers/dashboard/messages">
                           <DropdownMenuItem>
                             <MessageSquareCodeIcon />
                             Messages
@@ -223,22 +223,22 @@ const Navbar = ({ navItems, classNames }: Props) => {
                         </Link>
                         <DropdownMenuSeparator />
                         <Button
-                          variant='outline'
+                          variant="outline"
                           onClick={async () => {
-                            setLoading(true)
+                            setLoading(true);
                             const data = await signOut({
                               redirect: false,
-                              callbackUrl: '/',
-                            })
-                            setLoading(false)
-                            console.log(loading)
+                              callbackUrl: "/",
+                            });
+                            setLoading(false);
+                            console.log(loading);
 
                             if (data.url) {
-                              router.push(data.url)
+                              router.push(data.url);
                             }
                           }}
                         >
-                          <DropdownMenuItem className='hover:bg-transparent'>
+                          <DropdownMenuItem className="hover:bg-transparent">
                             <LogOut />
                             Log Out
                           </DropdownMenuItem>
@@ -247,13 +247,16 @@ const Navbar = ({ navItems, classNames }: Props) => {
                       </DropdownMenuContent>
                     </DropdownMenu>
                   ) : (
-                    <Link href='/sign-in'>SIGN IN</Link>
+                    <Link href="/sign-in">SIGN IN</Link>
                   )}
                 </div>
               </div>
-              {status !== 'authenticated' && (
-                <Link href='/vendors'>
-                  <Button size='lg'>Become A Vendor</Button>
+              {status !== "authenticated" && (
+                <Link
+                  href="https://memo-v3-vendor-web.vercel.app/"
+                  target="_blank"
+                >
+                  <Button size="lg">Become A Vendor</Button>
                 </Link>
               )}
             </div>
@@ -264,7 +267,7 @@ const Navbar = ({ navItems, classNames }: Props) => {
               <SheetTrigger asChild>
                 <Hamburger />
               </SheetTrigger>
-              <SheetContent side='left' className='w-full max-w-xs p-0'>
+              <SheetContent side="left" className="w-full max-w-xs p-0">
                 <MobileMenu navItems={navItems} />
               </SheetContent>
             </Sheet>
@@ -272,7 +275,7 @@ const Navbar = ({ navItems, classNames }: Props) => {
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
